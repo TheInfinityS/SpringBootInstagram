1c2fea8a4ce741cba5403072efbc11aa14b140df
TheInfinityS <infinityitf@gmail.com>
2023-01-30 14:08:48 +0600

    Gradle

---
+++ src/main/java/com/theinfinity/srpingbootinstagram/SrpingbootinstagramApplication.java
@@ -0,0 +1,13 @@
+package com.theinfinity.srpingbootinstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingbootinstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingbootinstagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theinfinity/srpingbootinstagram/SrpingbootinstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theinfinity.srpingbootinstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingbootinstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
